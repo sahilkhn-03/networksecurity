e9f975391e9e402aac5d0b075f0165cfbe70916d
sahilkhn-03 <sahilkhankhk5881@gmail.com>
2026-06-16 06:49:20 +0530

    model treaining Completed.

--- networksecurity/entity/artifact_entity.py
+++ networksecurity/entity/artifact_entity.py
@@ -26,9 +26,9 @@ class ClassificationMetricArtifact:
     precision_score: float
     recall_score: float
     
-# @dataclass
-# class ModelTrainerArtifact:
-#     trained_model_file_path: str
-#     train_metric_artifact: ClassificationMetricArtifact
-#     test_metric_artifact: ClassificationMetricArtifact
+@dataclass
+class ModelTrainerArtifact:
+    trained_model_file_path: str
+    train_metric_artifact: ClassificationMetricArtifact
+    test_metric_artifact: ClassificationMetricArtifact
 
